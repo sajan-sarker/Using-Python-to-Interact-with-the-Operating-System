0354f7de95453a8adbe0069ef9c2ac5f8dd24859
Sajan Kumer Sarker <sajansarker47@gmail.com>
2024-01-03 15:47:44 +0600

    unit test

--- Module 5/test_rearrange.py
+++ Module 5/test_rearrange.py
@@ -1,3 +1,21 @@
+import unittest
 from rearrange import rearrange_name
-print(rearrange_name("Lovelace, Ada"))
-print(rearrange_name("Ritchie, Dennis"))
+
+class TestRearrange(unittest.TestCase):
+    def test_basic(self):
+        testcase = "Lovelace, Ada"
+        expected = "Ada Lovelace"
+        self.assertEqual(rearrange_name(testcase), expected)
+    def test_empty(self):
+        testcase = ""
+        expected = ""
+        self.assertEqual(rearrange_name(testcase), expected)
+    def test_double_name(self):
+        testcase = "Hopper, Grace M."
+        expected = "Grace M. Hopper"
+        self.assertEqual(rearrange_name(testcase), expected)
+    def test_single_name(self):
+        testcase = "Voltaire"
+        expected = "Voltaire"
+        self.assertEqual(rearrange_name(testcase), expected)
+unittest.main()
